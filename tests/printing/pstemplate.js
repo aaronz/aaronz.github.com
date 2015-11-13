7dd9a041dd5f746135882658b6f9a15722bc99e3
Aaron Zhang <aaron.cheung@hotmail.com>
2015-11-14 00:21:18 +0800

    add printing test page.

---
+++ tests/printing/pstemplate.js
@@ -0,0 +1,16 @@
+﻿
+function HandleError(message, url, line)
+{
+var str = L_Dialog_ErrorMessage + "\n\n"
++ L_ErrorNumber_Text + line + "\n"
++ message;
+alert (str);
+window.close();
+return true;
+}
+function BodyOnload ()
+{
+Printer.showPageSetupDialog();
+window.close();
+}
+window.onerror = HandleError;

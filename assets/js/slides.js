f2a134ee77fa8b32a3ece3352a97d1314b3a5dc4
Aaron Zhang <aaron.cheung@hotmail.com>
2016-02-05 18:53:10 +0800

    refine page formats and add tools page.

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -300,18 +300,18 @@ jQuery(document).ready(function($) {
 
     //responsive code begin
     //you can remove responsive code if you don't want the slider scales while window resizing
-    // function ScaleSlider() {
-    //     var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
-    //     if (refSize) {
-    //         refSize = Math.min(refSize, 800);
-    //         jssor_1_slider.$ScaleWidth(refSize);
-    //     } else {
-    //         window.setTimeout(ScaleSlider, 30);
-    //     }
-    // }
-    // ScaleSlider();
-    //$(window).bind("load", ScaleSlider);
-    //$(window).bind("resize", ScaleSlider);
-    //$(window).bind("orientationchange", ScaleSlider);
+    function ScaleSlider() {
+        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+        if (refSize) {
+            refSize = Math.min(refSize, 624);
+            jssor_1_slider.$ScaleWidth(refSize);
+        } else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    }
+    ScaleSlider();
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
     //responsive code end
 });
